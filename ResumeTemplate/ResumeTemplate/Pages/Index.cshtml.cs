e902fafc0b42c7bec68e5e91c5d87eab700538df
Mohammad Sami Alam <sami.alam10@gmail.com>
2020-09-01 22:00:36 -0600

    initialize a new instance of the class Project

---
+++ ResumeTemplate/ResumeTemplate/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ResumeTemplate.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string FullName { get; set; }
+        public string LinkedInUsername { get; set; }
+        public int YearsOfExperience { get; set; }
+        public List<string> Languages { get; set; }
+
+        public void OnGet()
+        {
+            FullName = "Mohammad Sami Alam";
+            LinkedInUsername = "";
+            YearsOfExperience = 3;
+            Languages = new List<string> {
+                "C#",
+                "Javascript",
+                "Java",
+                "Python"
+
+            };
+
+        }
+    }
+}
---
+++ ResumeTemplate/ResumeTemplate/Pages/Projects.cshtml.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ResumeTemplate.Models;
+
+namespace ResumeTemplate.Pages
+{
+    public class ProjectsModel : PageModel
+    {
+        public List<Project> Projects { get; set; }
+
+		
+        public string GithubUsername { get; set; }
+        public void OnGet()
+        {
+			GithubUsername = "";
+			Projects = new List<Project> {
+				new Project()
+				{
+					Title = "Explorella",
+					Description = "A large scale travelling application built to help plan your destination and itinerary on your next trip! Built on React, Node, Express, and MongoDB.",
+					Year = 2020,
+					Technologies = new List<string> {
+						"React",
+						"Node",
+						"Express",
+						"MongoDB"
+					}
+				},
+				new Project()
+				{
+					Title = "Cypher Clothing",
+					Description = "A large scale e-commerce clothing store application. Built with Razor Pages and SQLite.",
+					Year = 2020,
+					Technologies = new List<string> {
+						"Razor Pages",
+						"SQLite",
+					}
+				},
+				new Project()
+				{
+					Title = "Password Box",
+					Description = "A password manager Chrome plugin used to create and safely store all your passwords! Built with Javascript and HTML.",
+					Year = 2020,
+					Technologies = new List<string> {
+						"Javascript",
+						"HTML",
+					}
+				},
+				new Project()
+				{
+					Title = "Spotivize",
+					Description = "Spotivize is a music platform that uses geolocation to track when your favorite artists are playing in your local city! Built with Node, Express, React and MongoDB.",
+					Year = 2020,
+					Technologies = new List<string> {
+						"Express",
+						"React",
+						"MongoDB"
+					}
+				}
+			};
+		}
+    }
+}
